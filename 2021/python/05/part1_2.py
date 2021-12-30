fedd4571c40e46c107856fae8a0c79f01e471e46
Petro Verkhogliad <vpetro@gmail.com>
2021-12-30 14:25:23 -0500

    [python] Refactor data reading in 2021.5

--- 2021/python/05/part1_2.py
+++ 2021/python/05/part1_2.py
@@ -3,13 +3,7 @@ from collections import defaultdict
 
 def read_data(input_filename: str):
     fh = resources.open_text('resources', input_filename)
-    lines = []
-    for line in fh.readlines():
-        start, finish = line.split(' -> ')
-        x1, y1 = [int(i) for i in start.split(',')]
-        x2, y2 = [int(i) for i in finish.split(',')]
-        lines.append([x1, y1, x2, y2])
-    return lines
+    return [list(map(int, line.replace(' -> ', ',').split(','))) for line in fh.readlines()]
 
 def get_change(i, j):
     mod = 0
@@ -38,7 +32,7 @@ def solve(data: list[list[int]], predicate):
     return sum([value >= 2 for _, value in grid.items()])
 
 if __name__ == "__main__":
-    data = read_data("input")
+    data = read_data("test")
     result1 = solve(data, lambda x,y: 0 in (x,y))
     result2 = solve(data, lambda x,y: True)
     print(f'part 1 result: {result1}')
