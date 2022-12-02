81789bfc90b06bc7b4824e34d5610c8847ee0933
Petro Verkhogliad <vpetro@gmail.com>
2022-12-02 16:59:47 -0600

    [2022 scala] Solve day 01

--- 2022/scala/02/src/main/scala/Main.scala
+++ 2022/scala/02/src/main/scala/Main.scala
@@ -6,15 +6,60 @@ object App {
     def main(args: Array[String]): Unit = {
 
     val part = Integer.parseInt(args(0))
-    val inputFile = File(args(1))
-    Problem.solve(part, inputFile)
+    val inputFile = File(s"src/main/resources/${part}.txt")
+    val result = Problem.solve(inputFile)
+    println(result)
 
   }
 }
+
 
 object Problem {
-  def solve(part: Int, inputFile: File) = {
-    println(s"Excecuting ${part} with file: ${inputFile}")
+  def solve(inputFile: File) = {
+
+    val plays = inputFile.lineIterator.map(line => line.split(" ").toList match {
+      case (x::y::Nil) => (x, y)
+      case _ => ???
+    })
+
+    (plays.map { value =>
+      val (p1, p2) = value
+      calculate2(p1, p2)
+    }).sum
   }
 
+  def calculate1(p1: String, p2: String): Int = {
+      // draw
+      if (p1 == "A" && p2 == "X") { 1 + 3 }
+      else if (p1 == "B" && p2 == "Y") { 2 + 3 }
+      else if (p1 == "C" && p2 == "Z") { 3 + 3 }
+      // other
+      else if (p1 == "A" && p2 == "Y") { 2 + 6 }
+      else if (p1 == "A" && p2 == "Z") { 3 + 0 }
+      else if (p1 == "B" && p2 == "X") { 1 + 0 }
+      else if (p1 == "B" && p2 == "Z") { 3 + 6 }
+      else if (p1 == "C" && p2 == "X") { 1 + 6 }
+      else if (p1 == "C" && p2 == "Y") { 2 + 0 }
+      else ???
+    }
+
+  def calculate2(p1: String, p2: String): Int = {
+      // A = Rock (1)
+      // B = Paper (2)
+      // C = Scissors (3)
+      // X - lose (0), Y - draw (3) - Z - win (6)
+
+      if (p1 == "A" && p2 == "X") { 3 + 0 }
+      else if (p1 == "B" && p2 == "Y") { 2 + 3 }
+      else if (p1 == "C" && p2 == "Z") { 1 + 6 }
+      else if (p1 == "A" && p2 == "Y") { 1 + 3 }
+      else if (p1 == "A" && p2 == "Z") { 2 + 6 }
+      else if (p1 == "B" && p2 == "X") { 1 + 0 }
+      else if (p1 == "B" && p2 == "Z") { 3 + 6 }
+      else if (p1 == "C" && p2 == "X") { 2 + 0 }
+      else if (p1 == "C" && p2 == "Y") { 3 + 3 }
+      else ???
+    }
+
 }
+
--- 2022/scala/02/src/test/scala/Basic.scala
+++ 2022/scala/02/src/test/scala/Basic.scala
@@ -2,14 +2,33 @@ package org.vpetro
 
 import org.scalatest.funsuite.AnyFunSuite
 
+import Problem.calculate1
+import Problem.calculate2
+
 class BasicTest extends AnyFunSuite {
   test("sanity test should pass") {
     assert(true)
   }
 
   test("solve should be implemented") {
-    assert(false)
+    assert(true)
   }
 }
 
 
+class SampleInputTest extends AnyFunSuite {
+  test("sample input should produce 15") {
+    assert(calculate1("A", "Y") == 8)
+    assert(calculate1("B", "X") == 1)
+    assert(calculate1("C", "Z") == 6)
+  }
+} 
+
+class SampleInputTest2 extends AnyFunSuite {
+  test("sample input should produce 15") {
+    assert(calculate2("A", "Y") == 4)
+    assert(calculate2("B", "X") == 1)
+    assert(calculate2("C", "Z") == 7)
+  }
+
+}
